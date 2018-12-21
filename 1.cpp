39249ca0336876578bea378a7fab5016b580778e
Lahut Sriharun <lahut.s@ku.th>
2018-12-21 10:14:31 +0700

    add 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string infix, postfix = "";
+    char _operator;
+
+    getline(cin, infix);
+
+    int cnt = 0;
+
+    for(int i = 0; infix[i] != '\0'; i++){
+        if(infix[i] != '+' && infix[i] != '-' && infix[i] != ' '){
+            postfix += infix[i];
+            cnt += 1;
+            if(cnt > 1 && (_operator == '+' || _operator == '-') 
+                        && (infix[i+1] == ' ' || infix[i+1] == '\0')){
+                postfix += ' ';
+                postfix += _operator;
+            }
+        }
+
+        if(infix[i] == '+' || infix[i] == '-'){
+            postfix += ' ';
+            _operator = infix[i];
+        }
+
+    }
+
+    cout << postfix << endl;
+    return 0;
+}
